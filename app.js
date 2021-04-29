be625fd7156969390928b56a6b733870156e2fbb
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-04-29 23:47:28 +0800

    Heroku Deployment

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
-require("dotenv").config();
+if (process.env.NODE_ENV === "development") require("dotenv").config();
 const routes = require("./routes");
-const errorHandler = require('./middlewares/errorHandler');
+const errorHandler = require("./middlewares/errorHandler");
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(errorHandler);
